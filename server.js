214a8ad30069b49f518d84c0fd364abd7359935a
drorcohen314 <drorc216@gmail.com>
2023-11-18 01:16:43 +0200

    add serve

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/fe/browser'));
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/fe/browser/index.html'));});
+app.listen(process.env.PORT || 8080);
